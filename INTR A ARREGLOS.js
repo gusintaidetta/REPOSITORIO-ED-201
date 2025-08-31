d41df8c9804d0e9a2317102b6f30572bf9234f7a
Gustavo <gustavoarpestudent@gmail.com>
2025-08-31 11:20:21 -0700

    Create INTR A ARREGLOS.js
    Trabajo realizado el 28 de agosto por Gustavo Araujo Perez del Grupo 202

---
+++ INTR A ARREGLOS.js
@@ -0,0 +1,20 @@
+// Declaración
+let arreglo = [];
+
+
+// Inicialización
+let arreglo2 = [10, 20, 30, 40, 50];
+
+
+// Asignar valor
+arreglo2[0] = 99;
+
+
+// Recuperar valor
+let valor = arreglo2[2]; // 30
+
+
+// Recorrer arreglo
+for (let i = 0; i < arreglo2.length; i++) {
+    console.log("Índice " + i + " -> " + arreglo2[i]);
+}
